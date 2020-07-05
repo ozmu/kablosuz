e859386f568a3cfc5762d3d3c20dd274da8dc082
Muhammet Öztürk <muhammet.ozturk@bil.omu.edu.tr>
2020-07-05 22:42:31 +0300

    updates

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var wifi = require('node-wifi');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/wireless', function(request, response, next){
+  wifi.init({
+    iface: null
+  });
+
+  wifi.scan((error, networks) => {
+    if (error) {
+      response.send(`Error: ${error}`);
+      console.log('Error: ', error);
+    }
+    else {
+      response.send(`Networks: ${JSON.stringify(networks)}`);
+      console.log('Networks: ', networks);
+    }
+  })
+})
 
 module.exports = router;
