f43b118e55af64cfa004c084ed89d22befe42362
Muhammet Öztürk <muhammet@ozturk.com>
2020-07-31 16:30:26 +0300

    Routing updated

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 import App from './App.vue'
 import './quasar'
+
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
---
+++ client/src/router/routes.js
@@ -0,0 +1,10 @@
+import About from '../pages/About';
+
+const routes = [
+    {
+        path: '/about',
+        component: About
+    }
+]
+  
+export default routes;
