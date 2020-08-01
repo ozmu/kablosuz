a653a320781d48785591f23e6987d8b7ba9bde0d
Muhammet Öztürk <muhammet@ozturk.com>
2020-08-01 17:01:07 +0300

    Table updates

--- client/src/pages/Scan.vue
+++ client/src/pages/Scan.vue
@@ -1,12 +1,70 @@
 <template>
   <q-page padding>
-      Scan vue
+      <div class="q-pa-md">
+        <q-table :data="data" :columns="columns" row-key="name"/>
+      </div>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      columns: [
+        {
+          name: 'name',
+          required: true,
+          label: 'SSID',
+          align: 'left',
+          field: row => row.name,
+          format: val => `${val}`,
+          sortable: true,
+          classes: 'bg-grey-2 ellipsis',
+          style: 'max-width: 100px',
+          headerClasses: 'bg-primary text-white'
+        },
+        { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+        { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+        { name: 'protein', label: 'Protein (g)', field: 'protein' },
+        { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+        { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+        { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+      ],
+      data: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%'
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          sodium: 129,
+          calcium: '8%',
+          iron: '1%'
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          sodium: 337,
+          calcium: '6%',
+          iron: '7%'
+        }
+      ]
+    }
+  }
 }
 </script>
 
